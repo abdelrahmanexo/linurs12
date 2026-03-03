88220d1c68ee6edc5f050851fa0183db1ce73feb
abdelrahmanexo <83197925+abdelrahmanexo@users.noreply.github.com>
2026-03-03 23:35:40 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* إضافة كائن turbopack فارغ لإخبار المحرك أنك على علم بالتعارض 
-     وهذا سيسمح لـ Vercel بإتمام عملية الـ Build بنجاح.
-  */
-  turbopack: {},
-
   webpack: (config) => {
-    // هذا السطر يخبر ويب-باك بتجاهل مكتبة canvas
     config.resolve.alias.canvas = false;
-
-    // حلول إضافية قد تحتاجها لبعض الحزم الخاصة بترميز النصوص
     config.resolve.alias.encoding = false;
-
     return config;
   },
 };
