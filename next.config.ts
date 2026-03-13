d3d6eea00a87b5011101528c142056cd7358538c
abdelrahmanexo <83197925+abdelrahmanexo@users.noreply.github.com>
2026-03-13 16:33:54 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    // هذا السطر يخبر ويب-باك بتجاهل مكتبة canvas
-    config.resolve.alias.canvas = false;
-
-    // حلول إضافية قد تحتاجها لبعض الحزم الخاصة بترميز النصوص
-    config.resolve.alias.encoding = false;
-
-    return config;
+  turbopack: {
+    resolveAlias: {
+      canvas: "./empty-module.ts",
+      encoding: "./empty-module.ts",
+    },
   },
 };
 
